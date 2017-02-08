31c8a7b241c40303e89aa66dc903d8e2c1af2022
sdylanjeffery <sdylanjeffery@C218-1762.yccc.edu>
2017-02-08 18:31:08 -0500

    more logic

--- EG2/src/week2/Boat.java
+++ EG2/src/week2/Boat.java
@@ -4,9 +4,14 @@ public class Boat extends Vehicle{
 	boolean inWater = true;
 
 	public Boat(){
+		if(inWater != false){
 		setFuelType("Gas");
 		setMaxTopSpeed(40);
 		setNumberOfPassengers(4);
+		}else{
+		setMaxTopSpeed(0);
+		
+		}
 		
 	}
 	
@@ -14,7 +19,7 @@ public class Boat extends Vehicle{
 		if (inWater != false){
 			System.out.println("Vrooom");
 		}else{
-			System.out.println("I've got nowhere to go!");
+			System.out.println("You hear the engine but don't seem to be going anywhere...");
 		}
 	}
 
